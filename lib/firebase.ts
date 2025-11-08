0f0004ff03f0ba13440671860d819faa17941f70
moathssnck <myneww00@gmail.com>
2025-11-08 16:13:55 +0300

    sdsd

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,14 +3,12 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBnBBMgQGN5NOelo-1VA45pCQMjwVu5gLE",
-  authDomain: "gvsstc.firebaseapp.com",
-  databaseURL: "https://gvsstc-default-rtdb.firebaseio.com",
-  projectId: "gvsstc",
-  storageBucket: "gvsstc.firebasestorage.app",
-  messagingSenderId: "1029610207085",
-  appId: "1:1029610207085:web:3413ab7ae0fa03a9958c64",
-  measurementId: "G-G3R67QJ0DK"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
